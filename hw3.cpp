cb4fbb02d96f7e1a7c79390bafbce1816860394d
Valerio Marcelli <marcelli@seattleu.edu>
2018-05-20 10:43:54 -0700

    Finished off the rest of code

--- hw3.cpp
+++ hw3.cpp
@@ -1,6 +1,9 @@
 #include <semaphore.h>
 #include <pthread.h>
+#include <stdlib.h>
+#include <sstream>
 #include <iostream>
+#include <string>
 
 using namespace std;
 
@@ -11,98 +14,113 @@ sem_t patientReady;
 
 int numberOfFreeWRSeats = 0;
 
-struct patients {
+struct patients_t {
     int patientNum;
 };
 
 void* Dentist(void* arg) {
 
+    string dentistAquiring = "Dentist trying to acquire patient…";
+    string dentistAquiringSeat = "Dentist trying to acquire seatCountWriteAccess…";
+    string dentistReadyConsult = "Dentist ready to consult…";
+    string dentistReleasignSeats = "Dentist releasing seatCountWriteAccess…";
+    string dentistConsulting = "Dentist consulting patient…";
+
     while(true) {
-        cout << "Dentist trying to acquire patient…" << endl;
+        printf("%s\n", dentistAquiring.c_str());
         sem_wait(&patientReady);
-        cout << "Dentist trying to acquire seatCountWriteAccess…" << endl;
-
+        printf("%s\n", dentistAquiringSeat.c_str());
         sem_wait(&seatCountWriteAccess);
-        if(numberOfFreeWRSeats > 0) {
-            numberOfFreeWRSeats += 1;
-            cout << "Incremented free seats to " << numberOfFreeWRSeats << endl;
 
-            sem_post(&patientReady);
-            cout << "“Dentist releasing seatCountWriteAccess…" << endl;
-            sem_post(&seatCountWriteAccess);
-            cout << "Dentist consulting patient…" << endl;
-            sem_wait(&dentistReady);
-
-        }
-        else {
-            cout << "“Dentist releasing seatCountWriteAccess…" << endl;
-            sem_post(&seatCountWriteAccess);
-            cout << "Dentist consulting patient…" << endl;
-        }
+        //Add free seat
+        numberOfFreeWRSeats += 1;
+        string dentistIncrementing = "Incremented free seats to " 
+            + to_string(numberOfFreeWRSeats);
+        printf("%s\n", dentistIncrementing.c_str());
+        //Ready to consult
+        printf("%s\n", dentistReadyConsult.c_str());
+        sem_post(&dentistReady);
+        printf("%s\n", dentistReleasignSeats.c_str());
+        sem_post(&seatCountWriteAccess);
+        printf("%s\n", dentistConsulting.c_str());
+        //Talking to patient
     }
-
+    pthread_exit(0);
 }
 
-void* Customer(void * arg) {
-    struct patients *tempStruct = (struct patients) ptr;
+void* Customer(void* arg) {
+    /* Converting Struct */
+    struct patients_t *tempStruct = (struct patients_t*) arg;
     int threadID =  tempStruct->patientNum;
-
-    while(true) {
-        //Waitinf for a free seat
-        cout << "“Customer " << threadID << "trying to acquire seatCountWriteAccess…" << endl;
+    
+    while(true) {    
+        //Waiting for a free seat
+        string waitForSeat = "Customer " + to_string(threadID) 
+            + " trying to acquire seatCountWriteAccess…";
+        printf("%s\n", waitForSeat.c_str());
         sem_wait(&seatCountWriteAccess);
 
+        //Free seats still left
         if(numberOfFreeWRSeats > 0) {
             numberOfFreeWRSeats -= 1;
-            cout << "“Customer " << threadID << " seated; Remaining chairs=" << numberOfFreeWRSeats << endl;
-            cout << "Customer " << threadID << " notifying dentist patientReady…" << endl;
+            string customerSeated = "Customer " + to_string(threadID) 
+                + " seated; Remaining chairs=" + to_string(numberOfFreeWRSeats);
+            printf("%s\n", customerSeated.c_str());
+            string customerNotifyDentist = "Customer " + to_string(threadID) 
+                + " notifying dentist patientReady…";
+            printf("%s\n", customerNotifyDentist.c_str());
             sem_post(&patientReady);
-            cout << "Customer " << threadID << " releasing seatCountWriteAccess…" << endl;
+            string customerReleaseSeat = "Customer " + to_string(threadID) 
+                + " releasing seatCountWriteAccess…";
+            printf("%s\n", customerReleaseSeat.c_str());
             sem_post(&seatCountWriteAccess);
-            cout << "Customer " << threadID << " waiting for dentist…" << endl;
+            string customerWaitDentist = "Customer " + to_string(threadID) 
+                + " waiting for dentist…";
+            printf("%s\n", customerWaitDentist.c_str());
             sem_wait(&dentistReady);
-            cout << "Customer " << threadID << " consulting dentist…" << endl;
-
+            string customerConsulting = "Customer " + to_string(threadID) 
+                + " consulting dentist…";
+            printf("%s\n", customerConsulting.c_str());
         }
         //No free seats
         else {
-            cout << "Customer " << threadID << " leaving without consulting; no chairs available…" << endl;
+            string customerLeaving = "Customer " + to_string(threadID) 
+                + " leaving without consulting; no chairs available…";
+            printf("%s\n", customerLeaving.c_str());
             sem_post(&seatCountWriteAccess);
             //Leave without consulting
         }
-
     }
-
-}
-void* thread(void* arg) {
-    int threadID =  pthread_self();
-
-    //waiting
-    sem_wait(&mutex);
-    cout << "Inside of thread" << endl;
-
-    //critical section
-    cout << "Doing critical stuff " << threadID << endl;
-
-    //signal
-    cout << "Exiting thread" << endl;
-    sem_post(&mutex);
-
+    pthread_exit(0);
 }
 
-int main() {
-    sem_init(&mutex, 0 , 1);
+int main(int argc, char* argv[]) {
     sem_init(&dentistReady, 0, 1);
     sem_init(&seatCountWriteAccess, 0, 1);
     sem_init(&patientReady, 0, 1);
-    numberOfFreeWRSeats = 5;
-    int PATIENT_THREAD_NUM = 5;
+    stringstream stream(argv[1]);
+    stream >> numberOfFreeWRSeats;
 
-    pthread_t thread[PATIENT_THREAD_NUM];
+    stringstream stream2(argv[2]);
+    int argInput2 = 0;
+    stream2 >> argInput2;
+    const int PATIENT_THREAD_NUM = argInput2;
 
+    pthread_t *thread = new pthread_t[PATIENT_THREAD_NUM];
+    pthread_t dentistThread;
+
+    /* Create Dentist Thread */
+    int status = pthread_create(&dentistThread, NULL, Dentist, NULL);
+    if(status) {
+        string denErr = "Error when creating dentist thread";
+        printf("%s\n", denErr.c_str());
+        exit(1);
+    }
+
+    /* Create Patient Threads */
     for(int i=0; i < PATIENT_THREAD_NUM; i++) {
         /* Dynamic allocation of struct to pass data */
-        struct patients *tempStruct = new struct patients;
+        struct patients_t *tempStruct = new struct patients_t;
         tempStruct->patientNum = i;
 
         /*  Pass struct through threads */
@@ -110,8 +128,8 @@ int main() {
 
         /* If there is an error in creating a thread, exit */
         if(status) {
-            string err = "Error when creating thread #" + (to_string(i));
-            printf("\n"err"\n");
+            string err = "Error when creating patient thread #" + (to_string(i));
+            printf("%s\n", err.c_str());
             exit(1);
         }
 
@@ -121,9 +139,12 @@ int main() {
     for(int i=0; i < PATIENT_THREAD_NUM; i++) {
         pthread_join(thread[i], NULL);
     }
+    pthread_join(dentistThread, NULL);
     
-    sem_destroy(&mutex);
-
+    /* Destory Semaphores */
+    sem_destroy(&dentistReady);
+    sem_destroy(&seatCountWriteAccess);
+    sem_destroy(&patientReady);
 
     return 0;
 }
